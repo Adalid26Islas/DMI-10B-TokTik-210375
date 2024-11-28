8e1dbd947eef00f7a7faa82f17931e8cc2bf0ea2
Adalid26Islas <adalidislasquintero@gmail.com>
2024-11-27 20:57:23 -0600

    Implementación de un icono más que gira infinitamente

--- tok_tik_210375/lib/presentation/widgets/shared/video_buttons.dart
+++ tok_tik_210375/lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:tok_tik_210375/config/helpers/human_formats.dart';
 import 'package:tok_tik_210375/domain/entities/video_post.dart';
@@ -17,6 +18,12 @@ class VideoButtons extends StatelessWidget {
         _CustomIconButton( value: video.likes, iconColor: Colors.red, iconData: Icons.favorite, ),
         const SizedBox( height: 20 ),
         _CustomIconButton( value: video.views, iconData: Icons.remove_red_eye_outlined ),
+        const SizedBox( height: 20 ),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration( seconds: 3),
+          child: const _CustomIconButton( value: 0, iconData: Icons.play_circle_outline )
+        ),
       ],
     );
   }
